9cc4056b4faa9f4a95edd1c8656cf8bb519a0388
TomRingblom <t_ringblom@hotmail.com>
2022-05-03 15:21:50 +0200

    Added dependencies for SqlServer and Tools.

---
+++ EldoradoWebApi/Data/SqlContext.cs
@@ -0,0 +1,25 @@
+﻿using EldoradoApi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EldoradoApi.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+                
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<BrandEntity> Brands { get; set; } = null!;
+        public virtual DbSet<ColorEntity> Colors { get; set; } = null!;
+        public virtual DbSet<CouponEntity> Coupons { get; set; } = null!;
+        public virtual DbSet<OrderDetailsEntity> OrderDetails { get; set; } = null!;
+        public virtual DbSet<OrderEntity> Orders { get; set; } = null!;
+        public virtual DbSet<ProductEntity> Products { get; set; } = null!;
+        public virtual DbSet<SizeEntity> Sizes { get; set; } = null!;
+        public virtual DbSet<StatusEntity> Statuses { get; set; } = null!;
+    }
+}
