747c4bcc018ba7b8e4810782393b6e69b4fe7538
TomRingblom <t_ringblom@hotmail.com>
2022-05-15 11:40:29 +0200

    Added Azure AD B2C Authentication.

--- EldoradoWebApi/Program.cs
+++ EldoradoWebApi/Program.cs
@@ -1,13 +1,21 @@
 using EldoradoWebApi;
 using EldoradoWebApi.Data;
 using EldoradoWebApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var cString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<SqlContext>(x => x.UseSqlServer(cString));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(options =>
+    {
+        builder.Configuration.Bind("AzureAdB2C", options);
+    }, options => { builder.Configuration.Bind("AzureAdB2C", options); });
 builder.Services.AddScopedServices();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,7 +34,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
