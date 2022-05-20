417bab815690e71a82ae85d30083949ed9af4197
Michael S <Michael.Stojanovic@hotmail.com>
2022-05-20 11:41:42 +0200

    testing cors change

--- EldoradoWebApi/Program.cs
+++ EldoradoWebApi/Program.cs
@@ -33,7 +33,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
 app.UseAuthentication();
 app.UseAuthorization();
 
